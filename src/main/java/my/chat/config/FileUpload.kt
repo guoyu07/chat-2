fa2b44712a7c4e1a1383089578102b20027f149f
bourne.lyu <bourne.lyu@gmai.com>
2017-07-20 15:35:03 +0800

    add markdown

---
+++ src/main/java/my/chat/config/FileUpload.kt
@@ -0,0 +1,12 @@
+package my.chat.config
+
+/**
+ * @Title: ${file_name}
+ * @Description: ${todo}
+ * @author lyu lyusantu@gmail.com
+ * @date ${date} ${time}
+ * @version V1.0
+ */
+class FileUpload{
+
+}
